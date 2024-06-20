a2b9962fe3d1ed82bc52446141299740a00eccdd
tah384 <115026575+tah384@users.noreply.github.com>
2024-06-20 12:23:06 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const start = async () => {
     await bot.init()
     //bot.logger.info('⏳ Database syncing!')
     await bot.DATABASE.sync()
-    await bot.connect()
+    await bot.connect(mongodb+srv://startouch47:<password>@cluster0.htss7hg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0)
   } catch (error) {
     Debug.error(error);
     start();
